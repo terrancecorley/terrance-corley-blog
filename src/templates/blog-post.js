e035414c6c2ff69ceef35cbe98d1caff016ae58b
Terrance Corley <terrancecorley@outlook.com>
2019-04-28 16:18:42 -0700

    testing staticman implementation

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,19 +13,19 @@ export default ({ data }) => {
                 <meta name="description" content={post.frontmatter.content} />
             </Helmet>
             <div
-            css={css`
-                background-color: #fdfdfd;
+                css={css`
+                    background-color: #fdfdfd;
 
-                a {
-                    color: #2fa7bb;
-                    transition: color 0.2s ease-out;
+                    a {
+                        color: #2fa7bb;
+                        transition: color 0.2s ease-out;
 
-                    :hover,
-                    :focus {
-                        color: #227280;
+                        :hover,
+                        :focus {
+                            color: #227280;
+                        }
                     }
-                }
-            `}
+                `}
             >
                 <h1
                 css={css`
@@ -36,6 +36,12 @@ export default ({ data }) => {
                 </h1>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
+            <form method="POST" action="https://api.staticman.net/v2/entry/terrancecorley/blog/gh-pages/comments">
+                <label><input name="fields[name]" type="text" />Name</label>
+                <label><input name="fields[email]" type="email" />E-mail</label>
+                <label><textarea name="fields[message]"></textarea>Message</label>
+                <button type="submit">Submit</button>
+            </form>
         </Layout>
     )
 }
