9e98082ef0bde4d63a9529cb7e8ec371a8675503
Terrance Corley <terrancecorley@outlook.com>
2019-09-10 21:08:31 -0700

    removed commenting

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -28,20 +28,14 @@ export default ({ data }) => {
                 `}
             >
                 <h1
-                css={css`
+                    css={css`
                     color: #2fa7bb !important;
                 `}
                 >
-                {post.frontmatter.title}
+                    {post.frontmatter.title}
                 </h1>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </div>
-            <form method="POST" action="https://api.staticman.net/v3/entry/terrancecorley/blog/gh-pages/comments">
-                <label><input name="fields[name]" type="text" />Name</label>
-                <label><input name="fields[email]" type="email" />E-mail</label>
-                <label><textarea name="fields[message]"></textarea>Message</label>
-                <button type="submit">Submit</button>
-            </form>
         </Layout>
     )
 }
